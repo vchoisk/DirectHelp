974162c38316b654f03b5597f700d02bbaace922
Victor Choi <vchoisk@gmail.com>
2020-03-20 14:25:05 +0900

    :seedling: 초기 템플릿 추가
    react-static 템플릿을 추가하였습니다.

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,32 @@
+// Imports
+import plugin0 from '/Users/seungkyuchoi/projects/DirectHelp/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/Users/seungkyuchoi/projects/DirectHelp/node_modules/react-static-plugin-typescript",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/seungkyuchoi/projects/DirectHelp/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/seungkyuchoi/projects/DirectHelp/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/Users/seungkyuchoi/projects/DirectHelp/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/Users/seungkyuchoi/projects/DirectHelp",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/seungkyuchoi/projects/DirectHelp/src/pages/404.tsx': require('/Users/seungkyuchoi/projects/DirectHelp/src/pages/404.tsx').default,
+'/Users/seungkyuchoi/projects/DirectHelp/src/pages/about.tsx': require('/Users/seungkyuchoi/projects/DirectHelp/src/pages/about.tsx').default,
+'/Users/seungkyuchoi/projects/DirectHelp/src/pages/blog.tsx': require('/Users/seungkyuchoi/projects/DirectHelp/src/pages/blog.tsx').default,
+'/Users/seungkyuchoi/projects/DirectHelp/src/pages/index.tsx': require('/Users/seungkyuchoi/projects/DirectHelp/src/pages/index.tsx').default,
+'/Users/seungkyuchoi/projects/DirectHelp/src/containers/Post': require('/Users/seungkyuchoi/projects/DirectHelp/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/seungkyuchoi/projects/DirectHelp/src/pages/404.tsx'
+
